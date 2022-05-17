74216076a4ddc03556e02169dcd46c171c01b1d7
Daniel <Danielwesterlund17@gmail.com>
2022-05-18 00:47:52 +0200

    init

---
+++ src/main/java/com/example/iiii142xwebb/GreetingController.java
@@ -0,0 +1,22 @@
+package com.example.iiii142xwebb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class GreetingController {
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+    @GetMapping("/hello")
+    public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+}
---
+++ src/main/java/com/example/iiii142xwebb/Iiii142XWebbApplication.java
@@ -0,0 +1,15 @@
+package com.example.iiii142xwebb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Iiii142XWebbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Iiii142XWebbApplication.class, args);
+    }
+
+
+
+}
